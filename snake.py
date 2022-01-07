4e4fccdc037b96b545439e24b59f28c0b7365281
avaneeshweb <97124080+avaneeshweb@users.noreply.github.com>
2022-01-06 20:56:10 -0600

    more fixes on movement script

--- snake.py
+++ snake.py
@@ -3,32 +3,20 @@ import turtle as t
 import keyboard
 import time
 def up():
-    if check_loss() == -1:
-        print("you lost")
-    else:
-        if t.heading()!="down":
-            t.seth(90)
+    if t.heading()!="down":
+        t.seth(90)
 
 def down():
-    if check_loss() == -1:
-        print("you lost")
-    else:
-        if t.heading() != "up":
-            t.seth(270)
+    if t.heading() != "up":
+        t.seth(270)
 
 def left():
-    if check_loss() == -1:
-        print("you lost")
-    else:
-        if t.heading() != "right":
-            t.seth(180)
+    if t.heading() != "right":
+        t.seth(180)
 
 def right():
-    if check_loss() == -1:
-        print("you lost")
-    else:
-        if t.heading()!="left":
-            t.seth(0)
+    if t.heading()!="left":
+        t.seth(0)
 def move_turtle():
     t.forward(26)
 
@@ -45,9 +33,31 @@ t.turtlesize(1.3)
 go_to(-(442/2), -(441/2))
 t.lt(90)
 
-def check_loss():
+def check_loss(x, y):
     if x == (208) or x == -208 or  y == (208) or  y == -208:
         return -1
+def move():
+    if t.heading()==90:
+        if check_loss(t.xcor(), t.ycor()+26)==-1:
+            t.setpos(0,0)
+        else:
+            t.sety(t.ycor()+0.1)
+    if t.heading()==270:
+        if check_loss(t.xcor(), t.ycor()-26)==-1:
+            t.setpos(0,0)
+        else:
+            t.sety(t.ycor()-26)
+    if t.heading()==0:
+        if check_loss(t.xcor()+26, t.ycor())==-1:
+            t.setpos(0,0)
+        else:
+            t.setx(t.xcor()+26)
+    if t.heading()==180:
+        if check_loss(t.xcor()-26, t.ycor())==-1:
+            t.setpos(0,0)
+        else:
+            t.setx(t.xcor()-26)
+
 
 #makes a grid for the board
 for i in range(4):
@@ -59,9 +69,7 @@ for i in range(4):
         t.left(90)
     t.right(90)
 t.speed(100)
-x=0
-y=0
-go_to(x,y)
+go_to(0,0)
 """
 def go_up():
     if check_loss() == -1:
@@ -93,14 +101,19 @@ def go_left():
     time.sleep(0.25)"""
 
 
+
 t.listen()
 
 t.onkey(up, "Up")  # This will call the up function if the "Left" arrow key is pressed
 t.onkey(down, "Down")
 t.onkey(left, "Left")
 t.onkey(right, "Right")
+
+
 while True:
-    move_turtle()
+    t.update()
+
+    move()
 t.mainloop()
 
 """
